f2c109b87834d46762c4c2434ead5aa9268511fc
TimJoePat <TimJoePat88@cox.net>
2024-03-09 00:22:00 -0700

    305 Using Stuyles with React Components - COMPLETE

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -6,6 +6,7 @@ import {
   useNavigation,
 } from "react-router-dom";
 import { createOrder } from "../../services/apiRestaurant";
+import Button from "../../ui/Button";
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -106,16 +107,9 @@ function CreateOrder() {
             name="cart"
             value={JSON.stringify(cart)}
           />
-          <button disabled={isSubmitting} 
-          className="bg-yellow-400 uppercase font-semibold
-          text-stone-800 px-4 py-4 inline-block
-          tracking-wide rounded-full hover:bg-yellow-300
-          transition-colors selection:duration-300
-          focus:outline-none focus:ring focus:ring-yellow-300
-          focus:bg-yellow-300 focus:ring-offset-2
-          disabled:cursor-not-allowed">
+          <Button disabled={isSubmitting}>
             {isSubmitting ? "Placing order..." : "Order now"}
-          </button>
+          </Button>
         </div>
       </Form>
     </div>
